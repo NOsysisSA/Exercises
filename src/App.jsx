4f7552b14814a84ec9905e33e2d055c44d0dfb86
Misha Koziura <misha.mehanuk2000@gmail.com>
2023-08-25 00:22:00 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./Components/Header/Header"
+import Content from "./Components/Content/Content"
+import Footer from "./Components/Footer/Footer"
+function App() {
+  return (
+    <div>
+      <Header/>
+      <Content/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
